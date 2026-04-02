63bd81a97b502234e15830f084d761a5261201da
Masanori Shioya <mash180sx@gmail.com>
2026-04-02 18:46:17 +0900

    Fix Android status bar overlap

--- mobile/android/app/src/main/java/com/aitarotchat/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/aitarotchat/app/MainActivity.java
@@ -1,5 +1,16 @@
 package com.aitarotchat.app;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Android 15+/edge-to-edge 環境でも WebView がステータスバーや
+        // ディスプレイカットアウトの下に潜らないようにする。
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+    }
+}
